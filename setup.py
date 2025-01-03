6147bdb77193219fa6a8415c6836ba7b3e26b32b
Dataworld123 <nishantsingh801102@gmail.com>
2025-01-03 14:59:07 +0530

    project structured ready  and venv

---
+++ setup.py
@@ -0,0 +1,10 @@
+
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Nishant kumar",
+    author_email="nishantsingh801102@gmail.com",
+    packages=find_packages()
+)
